239ba732ca38d5d61fb1cfa01c4cadc064c8cf30
leo <borrellil@msoe.edu>
2024-09-11 16:27:42 -0500

    Moved Driver to new package - forgot to commit!

--- src/borrellil/Driver.java
+++ src/borrellil/Driver.java
@@ -2,10 +2,10 @@
  * Course: CSC1020
  * Lab 2 - Exceptions
  * Main Driver class
- * Name: FIXME
- * Last Updated: FIXME
+ * Name: Leonardo Borrelli
+ * Last Updated: 9/11/2024
  */
-package username;
+package borrellil;
 
 public class Driver {
 
